b8afcf2cf185161fb2347db6a6e0c206db863027
koreyoshilower <1490852514@qq.com>
2025-01-22 22:55:59 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -63,6 +63,9 @@ sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/haiibo/OpenWrt'|g" p
 # sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" package/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|ARMv8|ARMv8_PLUS|g" package/luci-app-amlogic/root/etc/config/amlogic
 
+# Lucky
+git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+
 # SmartDNS
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
